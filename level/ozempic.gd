ce90e5c663a929d84dabdc4900e31ea2353c9ee1
Mitch Helbling <mitch.helbling@gmail.com>
2025-04-26 15:29:03 -0500

    ozempic gd

---
+++ level/ozempic.gd
@@ -0,0 +1,13 @@
+class_name Ozempic
+extends Area2D
+## Collectible that disappears when the player touches it.
+
+
+@onready var animation_player := $AnimationPlayer as AnimationPlayer
+
+
+func _on_body_entered(body: Node2D) -> void:
+	#animation_player.play(&"picked")
+	(body as Player).coin_collected.emit()
+	(body as Player).CHEESEBURGERS -= 1
+	(body as Player).scale_player(0.9)

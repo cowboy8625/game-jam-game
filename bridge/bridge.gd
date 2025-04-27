4a90f12d60c662eb3708a992edccd05624d8ec3f
Caleb Kuss <caleb.kuss@coschedule.com>
2025-04-27 14:18:37 -0500

    finish up bridge logic ugly version

--- bridge/bridge.gd
+++ bridge/bridge.gd
@@ -1,33 +1,44 @@
 extends RigidBody2D
 
-var player = null
-var player_on_bridge = false
+var player: Player = null
+var player_on_bridge := false
+
 @export var crack_sound: AudioStream
 @export var break_sound: AudioStream
-@onready var audio_player = $AudioStreamPlayer
+@onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var cracked_sprite: Sprite2D = $CrackedSprite
 
-var crack_sound_played = false
-var break_sound_played = false
+var crack_sound_played := false
+var break_sound_played := false
 
 func _ready():
 	freeze = true
-	if !has_node("AudioStreamPlayer"):
+	if cracked_sprite:
+		cracked_sprite.visible = false
+	
+	if !audio_player:
 		var new_audio = AudioStreamPlayer.new()
 		new_audio.name = "AudioStreamPlayer"
 		add_child(new_audio)
 		audio_player = new_audio
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body:
-		player = (body as Player) 
+	if body is Player:
+		player = body
 		player_on_bridge = true
-		update_bridge_state(player.CHEESEBURGERS)
+		check_player_weight()
 
-func _process(delta):
+
+func _physics_process(delta):
 	if player_on_bridge and player:
-		update_bridge_state(player.CHEESEBURGERS)
+		check_player_weight()
 
-func update_bridge_state(burger_count: int) -> void:
+func check_player_weight() -> void:
+	if not player_on_bridge or player == null:
+		return
+	
+	var burger_count = player.CHEESEBURGERS
+	
 	if burger_count >= 10 and !break_sound_played:
 		break_bridge()
 	elif burger_count >= 5 and !crack_sound_played:
@@ -38,8 +49,14 @@ func crack_bridge() -> void:
 		audio_player.stream = crack_sound
 		audio_player.play()
 		crack_sound_played = true  
+	if cracked_sprite:
+		cracked_sprite.visible = true
 
-func break_bridge() -> void:	
+func break_bridge() -> void:
+	print(player_on_bridge, player)
+	if not player_on_bridge or player == null:
+		return
+	
 	if break_sound and audio_player:
 		audio_player.stream = break_sound
 		audio_player.play()
@@ -48,3 +65,9 @@ func break_bridge() -> void:
 	freeze = false
 	await get_tree().create_timer(1.0).timeout
 	queue_free()
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Player:
+		player = body
+		player_on_bridge = false

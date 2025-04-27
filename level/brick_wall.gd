d211f36129b957a23ee0eeb26b4d4d4e2b10af00
cowboy8625 <cowboy8625@yahoo.com>
2025-04-27 10:04:52 -0500

    break sound on brick wall

--- level/brick_wall.gd
+++ level/brick_wall.gd
@@ -1,7 +1,15 @@
 extends Node
 @export var breakingPoint :int = 1
 
+@onready var break_wall_sound = $WallBreakSound
+
+func play_sound_and_free(sound: AudioStreamPlayer2D):
+	sound.get_parent().remove_child(sound)
+	get_tree().current_scene.add_child(sound)
+	sound.play()
+	sound.connect("finished", Callable(sound, "queue_free"))
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body as Player and body.CHEESEBURGERS > breakingPoint:
+		play_sound_and_free(break_wall_sound)
 		queue_free()

ac35116eb424cf9029923d976af12e6ce69fc78c
cowboy8625 <cowboy8625@yahoo.com>
2025-04-27 11:57:14 -0500

    got between 1 and 2

--- levels/level2/state.gd
+++ levels/level2/state.gd
@@ -2,6 +2,7 @@ extends State
 
 class_name Level2State
 @export var next_level = "Level3"
+@export var prev_level = "Level1"
 
 func enter_state(machine: GameStateMachine):
 	machine.current_level = preload("res://levels/level2/level.tscn").instantiate()
@@ -28,3 +29,6 @@ func input_state(_machine: GameStateMachine, _event: InputEvent) -> void:
 
 func _on_go_to_next_level_body_entered(body: Node2D) -> void:
 	EventHandler.emit_signal(&"change_state", next_level)
+
+func _on_go_to_last_level_body_entered(body: Node2D) -> void:
+	EventHandler.emit_signal(&"change_state", prev_level)

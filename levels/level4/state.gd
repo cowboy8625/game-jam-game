cea8bd93311c6ee4f3418d797d156e651b2dede2
chadhelbling <chad.helbling.1@gmail.com>
2025-04-27 17:08:42 -0500

    chore: removes player state from level 4

--- levels/level4/state.gd
+++ levels/level4/state.gd
@@ -5,15 +5,10 @@ class_name Level4State
 
 func enter_state(machine: GameStateMachine):
 	machine.current_level = preload("res://levels/level4/level.tscn").instantiate()
-	var player = machine.current_level.get_node("Player")
-	player.CHEESEBURGERS = machine.player_stats["cheese_burger_count"]
 	machine.add_child(machine.current_level)
 
 func exit_state(machine: GameStateMachine) -> void:
-	var player = machine.current_level.get_node("Player")
-	player.get_parent().remove_child(player)
 	machine.current_level.queue_free()
-	machine.player_stats["cheese_burger_count"] = player.CHEESEBURGERS
 
 func process_state(_machine: GameStateMachine, _delta: float) -> void:
 	pass
